fc13f67dfd34e8f2e424212befe0f2112720ada7
William Gomes <52439569+williamjayjay@users.noreply.github.com>
2021-05-28 02:06:27 -0300

    frontend web started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((res) => {
+      console.log(res);
+      setProjects(res.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    // console.log(projects);
+
+    const response = await api.post("projects", {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "William",
+    });
+
+    const reqProject = response.data;
+
+    // console.log("novo dado: ", response.data);
+    setProjects([...projects, reqProject]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {projects.map((proj) => (
+          <li key={proj.id}>{proj.title}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Add Projeto
+      </button>
+    </>
   );
 }
 
